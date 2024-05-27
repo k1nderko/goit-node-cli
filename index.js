9fde13ee841ea3ab5d8836240c8e85a62e97fca3
k1nderko <107281239+k1nderko@users.noreply.github.com>
2024-05-27 14:07:24 +0300

    p2-3-4

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { program } from "commander";
+import { listContacts, getContactById, removeContact, addContact } from "./contacts.js";
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -10,24 +12,23 @@ program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
-      break;
+      const allContacts = await listContacts();
+      return console.table(allContacts);
 
     case "get":
-      // ... id
-      break;
+      const getContact = await getContactById(id);
+      return console.log(getContact);
 
     case "add":
-      // ... name email phone
-      break;
+      const newContact = await addContact(name, email, phone);
+       return console.log(newContact);
 
     case "remove":
-      // ... id
-      break;
+      const deleteContact = await removeContact(id);
+      return console.log(deleteContact);
 
     default:
       console.warn("\x1B[31m Unknown action type!");
